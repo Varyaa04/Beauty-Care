92d65b2ec4dfd05c4402539fb536f10624ce4048
Varvara Fedorova <fedorova04_varya@mail.ru>
2024-05-15 22:35:38 +0300

    реализован поиск по названию товара, фильтрация по категории и сортировка по цене

--- Beauty&Care/goods/beautyGoodsPages.xaml.cs
+++ Beauty&Care/goods/beautyGoodsPages.xaml.cs
@@ -35,6 +35,78 @@ namespace Beauty_Care.goods
             }
             ListGoods.ItemsSource = goods;
 
+            comboSort.Items.Add("По возрастанию цены");
+            comboSort.Items.Add("По убыванию цены");
+
+            comboFilter.ItemsSource = Entities.GetContext().category.Select(x =>  x.Name).ToList();
+
+
+        }
+
+
+        beautyGoods[] findGoods()
+        {
+            List<beautyGoods> products = AppConnect.modeldb.beautyGoods.ToList();
+            var prodictall = products;
+
+            if(textboxSearch != null)
+            {
+                products = products.Where(x => x.nameGoods.ToLower().Contains(textboxSearch.Text.ToLower())).ToList();
+            }
+
+            if(comboSort.SelectedIndex > 0) 
+            { 
+                switch (comboSort.SelectedIndex)
+                {
+                    case 0:
+                        products = products.OrderBy(x => x.price).ToList();
+                        break;
+                    case 1:
+                        products = products.OrderByDescending(x => x.price).ToList();
+                        break;
+                }
+            }
+
+            if(comboFilter.SelectedIndex > 0)
+            {
+                switch(comboFilter.SelectedIndex)
+                {
+                    case 0:
+                        products = products.Where(x => x.category == 1).ToList();
+                        break;
+                    case 1:
+                        products = products.Where(x => x.category == 2).ToList();
+                        break;
+                    case 2:
+                        products = products.Where(x => x.category == 3).ToList();
+                        break;
+                }
+            }
+
+            if (products.Count > 0)
+            {
+                tbCounter.Text = "Найдено " + products.Count + " товаров";
+            }
+            else
+            {
+                tbCounter.Text = "Ничего не найдено";
+            }
+            ListGoods.ItemsSource = products;
+            return products.ToArray();
+        }
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void comboSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void buttonSearch_Click(object sender, RoutedEventArgs e)
+        {
+            findGoods();
         }
     }
 }
