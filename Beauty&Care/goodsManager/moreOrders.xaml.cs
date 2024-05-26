595213e3ad7a995fd32b1f1abe04de32e19cf1ca
Varvara Fedorova <fedorova04_varya@mail.ru>
2024-05-26 23:11:07 +0300

    чет сделала

--- Beauty&Care/goodsManager/moreOrders.xaml.cs
+++ Beauty&Care/goodsManager/moreOrders.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -21,45 +22,42 @@ namespace Beauty_Care.goodsManager
     /// </summary>
     public partial class moreOrders : Page
     {
-        private ordersManager _currentOrders = new ordersManager();
-
-        public moreOrders(ordersManager selectedOrders)
+        public moreOrders(ordersManager selectedorder)
         {
+            var curentorderid = selectedorder.idOrderManager;
+            var curentorderuser = selectedorder.orders.idUsers;
+            var curentorderstatus = selectedorder.orders.idStatus;
             InitializeComponent();
 
-            if (selectedOrders != null)
-            {
-                _currentOrders = selectedOrders;
-            }
-            DataContext = _currentOrders;
+            var moreorder = Entities.GetContext().ordersManager
+                               .Where(m => m.idOrder == curentorderid)
+                               .Select(m => m.idGoods)
+                               .ToList();
+            var goodsInorder = Entities.GetContext().beautyGoods
+                                         .Where(x => moreorder.Contains(x.idGoods))
+                                         .ToList();
+            listgoodsorder.ItemsSource = goodsInorder;
 
-            AppConnect.modeldb.ordersManager.ToArray();
+            var userlogin = Entities.GetContext().users
+                               .FirstOrDefault(s => s.idUser == curentorderuser);
+            labeluser.Content = userlogin.login;
 
+            labelId.Content = selectedorder.orders.idUsers;
 
-            comboStatus.ItemsSource = Entities.GetContext().status.Select(x => x.nameStatus).ToList();
+            var statusorder = Entities.GetContext().status
+                               .FirstOrDefault(s => s.idStatus == curentorderstatus);
+
+            labelstatus.Content = statusorder.idStatus;
+        }
+
+        private void ToOrders_Click(object sender, RoutedEventArgs e)
+        {
+            AppFrame.frameMain.Navigate(new managerOrders());
         }
 
         private void btnBack_Click(object sender, RoutedEventArgs e)
         {
             AppFrame.frameMain.GoBack();
         }
-
-        private void btnEdit_Click(object sender, RoutedEventArgs e)
-        {
-            if (_currentOrders.idOrder == 0)
-            {
-                Entities.GetContext().ordersManager.Add(_currentOrders);
-            }
-            try
-            {
-                Entities.GetContext().SaveChanges();
-                MessageBox.Show("Данные успешно изменены!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message.ToString());
-            }
-        }
     }
-}
 }
